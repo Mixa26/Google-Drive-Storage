1eff3d21df0d5b12e96d45aa399fd7efa9595b73
Mihajlo <djmixa26.hockey@gmail.com>
2022-10-28 20:00:29 +0200

    Creating a directory on the drive implemented.

--- src/main/java/DriveStorage.java
+++ src/main/java/DriveStorage.java
@@ -14,8 +14,11 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import error.StorageErrFactory;
+import error.types.StorageErrorType;
 
 import java.io.*;
 import java.security.GeneralSecurityException;
@@ -28,7 +31,12 @@ import java.util.List;
 public class DriveStorage implements Storage{
     //configuring DriveStorage to be singleton
     private static DriveStorage instance;
+    //root id which we use to identify the root folder
+    //it is setup in the createRoot
     private String rootId = "";
+    //setting the configuration(limitations)
+    //so we know the max amount of files,bytes...
+    private Configuration driveConfiguration;
 
     public static DriveStorage getInstance() {
         if (instance == null)
@@ -62,6 +70,11 @@ public class DriveStorage implements Storage{
      */
     private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE, DriveScopes.DRIVE_APPDATA, DriveScopes.DRIVE_FILE, DriveScopes.DRIVE_METADATA, DriveScopes.DRIVE_SCRIPTS);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    //MY VARIABLES
+    private String folderMimeType = "application/vnd.google-apps.folder";
+
+    private Drive service;
 
     /**
      * Creates an authorized Credential object.
@@ -97,7 +110,7 @@ public class DriveStorage implements Storage{
         try {
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
-            Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build();
+            service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build();
 
             //creating a empty folder called Drive root
             File folderMetadata = new File();
@@ -107,17 +120,23 @@ public class DriveStorage implements Storage{
             folderMetadata.setDriveId("root");
 
             java.io.File config = new java.io.File("files/configuration.json");
-            //config.createNewFile();
 
+            //writing the contents of configuration into a jason file
             FileWriter writer = new FileWriter("files/configuration.json");
             writer.write(configuration.toJson());
             writer.close();
-            
+
+            //creating metadata for configuration for the drive on the cloud
             File fileMetadata = new File();
             fileMetadata.setName("configuration.json");
             FileContent configContent = new FileContent("media/text",config);
 
+            //setting this Classes configuration
+            //so we know the limitations
+            driveConfiguration = configuration;
+
             try{
+                //setting up the folder and the configuration file for uploading to the drive cloud
                 File folder = service.files().create(folderMetadata).setFields("id").execute();
                 ArrayList<String> ParentsList = new ArrayList<>();
                 rootId = folder.getId();
@@ -129,101 +148,200 @@ public class DriveStorage implements Storage{
             }
             catch (GoogleJsonResponseException e)
             {
-                //TODO uradi exception handling ovde
-                System.err.println("Unable to upload file: " + e.getDetails());
-                throw e;
+                System.err.println("Something went wrong with Google Json respond.");
             }
         }
         catch (IOException | GeneralSecurityException e)
         {
-            //TODO uradi exception handling ovde
-            e.printStackTrace();
+            System.err.println("Something went wrong with preprocessing steps of converting the configuration file and creation of the Drive folder.");
+        }
+        return false;
+    }
+
+    private List<File> getFilesByName(String name, String nameAndMimeiType, Drive service)
+    {
+        List<File> files = new ArrayList<File>();
+
+        try {
+            String pageToken = null;
+            do {
+                FileList result;
+
+                if (name.equals("")) {
+                    result = service.files().list()
+                            .setQ(nameAndMimeiType)
+                            .setSpaces("drive")
+                            .setFields("nextPageToken, files(id, name, parents, mimeType)")
+                            .setPageToken(pageToken)
+                            .execute();
+                } else {
+                    result = service.files().list()
+                            .setQ(name)
+                            .setSpaces("drive")
+                            .setFields("nextPageToken, files(id, name, parents, mimeType)")
+                            .setPageToken(pageToken)
+                            .execute();
+                }
+
+                files.addAll(result.getFiles());
+
+                pageToken = result.getNextPageToken();
+            } while (pageToken != null);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error with getting files by name.");
+        }
+        return files;
+    }
+
+    private String getFileId(String path,String mimeiType, Drive service)
+    {
+        if (path.equals(""))return rootId;
+        String[] folder = path.split("/");
+
+        String parentID = rootId;
+
+        boolean badBath = true;
+
+        for (int i = 0; i < folder.length; i++)
+        {
+            String name = "name='" + folder[i] + "'";
+            String nameAndMimeiType = name + " and mimeType='" + mimeiType + "'";
+            ArrayList<File> files = (ArrayList<File>) getFilesByName("",nameAndMimeiType,service);
+            if (files.isEmpty())
+            {
+                StorageErrFactory.createError(StorageErrorType.NOT_A_DIRECTORY);
+                System.err.println(path + " <-- bad path");
+                return "";
+            }
+            for (int j = 0; j < files.size(); j++)
+            {
+                if (files.get(j).getParents().contains(parentID))
+                {
+                    parentID = files.get(j).getId();
+                    badBath = false;
+                    break;
+                }
+            }
+            if(badBath)
+            {
+                StorageErrFactory.createError(StorageErrorType.NOT_A_DIRECTORY);
+                System.err.println(path + " <-- bad path");
+                return "";
+            }
+            else
+            {
+                badBath = true;
+            }
+            files.clear();
+        }
+
+        return parentID;
+    }
+
+    @Override
+    public boolean createDir(String path, String name) {
+        if (rootId.equals(""))
+        {
+            StorageErrFactory.createError(StorageErrorType.NO_ROOT);
+            return false;
+        }
+        try {
+            String fileId = getFileId(path,folderMimeType,service);
+
+            File fileMetadata = new File();
+            fileMetadata.setName(name);
+            fileMetadata.setParents(Collections.singletonList(fileId));
+            fileMetadata.setMimeType("application/vnd.google-apps.folder");
+            try {
+                File file = service.files().create(fileMetadata)
+                        .setFields("id")
+                        .execute();
+                return true;
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("Error making a folder. Bad Json respond.");
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println("Something went wrong with creating a directory.");
         }
         return false;
     }
 
     @Override
-    public boolean createDir(String path, String name) {
+    public boolean createFiles(String s, String[] strings) {
         return false;
     }
 
     @Override
-    public boolean saveFile(String[] pathsFrom, String pathTo) {
+    public boolean delete(String[] strings) {
         return false;
     }
 
     @Override
-    public boolean deleteFile(String[] paths) {
+    public boolean relocateFiles(String[] strings, String s) {
         return false;
     }
 
     @Override
-    public boolean deleteDir(String[] dirs) {
+    public boolean download(String s, String s1) {
         return false;
     }
 
     @Override
-    public boolean relocateFiles(String[] paths) {
+    public boolean rename(String s, String s1) {
         return false;
     }
 
     @Override
-    public boolean download(String[] pathsFrom, String pathTo) {
-        return false;
-    }
-
-    @Override
-    public boolean rename(String name) {
-        return false;
-    }
-
-    @Override
-    public Metadata searchAllFilesInDir(String dirPath) {
+    public Metadata searchAllFilesInDir(String s) {
         return null;
     }
 
     @Override
-    public ArrayList<java.io.File> searchAllDirsInDir(String dirPath) {
+    public ArrayList<java.io.File> searchAllDirsInDir(String s) {
         return null;
     }
 
     @Override
-    public ArrayList<java.io.File> searchAllFilesInDirs(String dirPath) {
+    public ArrayList<java.io.File> searchAllFilesInDirs(String s) {
         return null;
     }
 
     @Override
-    public ArrayList<java.io.File> searchFilesByExt(String ext) {
+    public ArrayList<java.io.File> searchFilesByExt(String s, String s1) {
         return null;
     }
 
     @Override
-    public ArrayList<java.io.File> searchFileBySub(String substring) {
+    public ArrayList<java.io.File> searchFileBySub(String s) {
         return null;
     }
 
     @Override
-    public boolean dirContainsFiles(String[] names) {
+    public boolean dirContainsFiles(String s, String[] strings) {
         return false;
     }
 
     @Override
-    public java.io.File folderContainingFile(String name) {
+    public java.io.File folderContainingFile(String s) {
         return null;
     }
 
     @Override
-    public void sort(SortParamsEnum sortBy, boolean ascending) {
+    public void sort(SortParamsEnum sortParamsEnum, boolean b) {
 
     }
 
     @Override
-    public ArrayList<java.io.File> filesCreatedModifiedOnDate(String dirPath) {
+    public ArrayList<java.io.File> filesCreatedModifiedOnDate(String s) {
         return null;
     }
 
     @Override
-    public void filterSearchResult(boolean fullPath, boolean showSize, boolean showDateOfCreation, boolean showDateOfModification) {
+    public void filterSearchResult(boolean b, boolean b1, boolean b2, boolean b3) {
 
     }
 }
--- src/main/java/testiranje.java
+++ src/main/java/testiranje.java
@@ -1,5 +1,9 @@
 public class testiranje {
     public static void main(String[] args) {
         DriveStorage.getInstance().createRoot(new Configuration());
+        DriveStorage.getInstance().createDir("","folder");
+        DriveStorage.getInstance().createDir("folder","folder1");
+        DriveStorage.getInstance().createDir("folder","folder2");
+        DriveStorage.getInstance().createDir("folder/folder2","folder3");
     }
 }
